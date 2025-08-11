ada9fb4bf36690ac7848f5e40ca586afe6ad5fc3
S Manjunath Reddy <sheshireddyga.s@hcl.com>
2025-08-11 07:35:00 +0530

    HMS_13

--- src/Signup.java
+++ src/Signup.java
@@ -26,7 +26,6 @@ public class Signup extends javax.swing.JFrame {
         initComponents();
     }
 
-    @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
